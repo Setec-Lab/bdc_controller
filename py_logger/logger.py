610327e5aa73d2e7af991388db3b02b435a293e3
Juan J. Rojas <juanjorojash@gmail.com>
2021-08-19 15:22:32 -0600

    logger

--- py_logger/logger.py
+++ py_logger/logger.py
@@ -14,51 +14,45 @@ file_name = "data_" + date_string + ".csv"
 print("Saving to: ",end='')
 print(file_name) 
 
-ser = serial.Serial('COM3', baudrate=115200, bytesize=8, parity='N', stopbits=1, timeout=1.5, xonxoff=0, rtscts=0)
+ser = serial.Serial('COM4', baudrate=57600, bytesize=8, parity='N', stopbits=1, timeout=1.5, xonxoff=0, rtscts=0)
 ser.close()
 ser.open()
-
-start = b'\x00'
-prev = b'\x00'
-read = b'\x00'
-buffer = ''
+pub_flag = 1
+# start = b'\x00'
+# prev = b'\x00'
+# read = b'\x00'
+buffer = b'\x00'
 time.sleep(1)
-ser.write(b'n')
-ser.readline().decode() #discard it
-ser.write(b'\x1B')
-print(ser.readline().decode(),end='')
-answer = input("")
-if answer == 'y':
-      pub_flag = 1
-ser.write(answer.encode())
-#ser.write(b'n')
+ser.write(b'\x73') #turn it on 
+ser.read_until(b'\x01\x02') #discard it
+# print(ser.readline().decode(),end='')
+# answer = input("")
+# if answer == 'y':
+#       pub_flag = 1
+# ser.write(answer.encode())
+# #ser.write(b'n')
 data = []
 while True:
-      try:
-            ser.read_until(b'S').decode()
-            buffer = ser.read(5).decode()
-            speed = float(buffer)
-            status = ser.read(1).decode()
-            buffer = ser.read(5).decode()
-            pub_speed = float(buffer)
-            if pub_flag == 1:
-                  print(speed,end='\t')
-                  print(status,end='\t')
-                  print(pub_speed)
-            else:
-                  print(speed)
-            date = datetime.datetime.now()
-            time_stamp = date.strftime("%d/%m/%y %H:%M:%S")
-            #data.append([time_stamp, speed, status ,pub_speed])
-            with open(file_name,'a+',newline='') as f:
-                  writer = csv.writer(f, dialect='excel')
-                  if pub_flag == 1:
-                        writer.writerow([time_stamp, speed, status ,pub_speed])
-                  else:
-                       writer.writerow([time_stamp, speed])
+      ser.read_until(b'\x01\x02')
+      buffer = ser.read(2)
+      minutes = int.from_bytes(buffer, "big")
+      buffer = ser.read(2)
+      seconds = int.from_bytes(buffer, "big")
+      buffer = ser.read(2)
+      vbus = float(int.from_bytes(buffer, "big"))
+      buffer = ser.read(2)
+      ipva = float(int.from_bytes(buffer, "big"))
+      buffer = ser.read(2)
+      iloa = float(int.from_bytes(buffer, "big"))
+      if pub_flag == 1:
+            print(vbus,end='\t')
+            print(ipva,end='\t')
+      date = datetime.datetime.now()
+      time_stamp = date.strftime("%d/%m/%y %H:%M:%S")
+      #data.append([time_stamp, speed, status ,pub_speed])
+      with open(file_name,'a+',newline='') as f:
+            writer = csv.writer(f, dialect='excel')
+            writer.writerow([time_stamp, vbus, ipva, iloa])
 
-      except:
-            print("Keyboard Interrupt")
-            break
 
 ser.close()
